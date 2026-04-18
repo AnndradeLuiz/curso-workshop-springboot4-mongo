ae443b88ba7354fa2dea8c8ec1d76693aa6a5476
Luiz Eduardo Eleutério de Andrade <180665674+AnndradeLuiz@users.noreply.github.com>
2026-04-18 15:22:02 -0300

    Simple query using query methods

--- src/main/java/com/luiz/workshop_mongo/resources/exceptions/PostResource.java
+++ src/main/java/com/luiz/workshop_mongo/resources/exceptions/PostResource.java
@@ -1,12 +1,14 @@
 package com.luiz.workshop_mongo.resources.exceptions;
 
 import com.luiz.workshop_mongo.domain.Post;
+import com.luiz.workshop_mongo.resources.util.URL;
 import com.luiz.workshop_mongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -19,6 +21,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titleserarch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
