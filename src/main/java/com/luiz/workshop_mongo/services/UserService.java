6035e09ab5dec3d54dacef1c9c8c759aa2b894e1
Luiz Eduardo Eleutério de Andrade <180665674+AnndradeLuiz@users.noreply.github.com>
2026-04-18 11:51:27 -0300

    User insertion using POST

--- src/main/java/com/luiz/workshop_mongo/services/UserService.java
+++ src/main/java/com/luiz/workshop_mongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.luiz.workshop_mongo.services;
 
 import com.luiz.workshop_mongo.domain.User;
+import com.luiz.workshop_mongo.dto.UserDTO;
 import com.luiz.workshop_mongo.repository.UserRepository;
 import com.luiz.workshop_mongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
