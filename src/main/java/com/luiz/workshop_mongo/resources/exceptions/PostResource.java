6b703a75f48678005a160a9b218d4d4dedff60e9
Luiz Eduardo Eleutério de Andrade <180665674+AnndradeLuiz@users.noreply.github.com>
2026-04-18 15:38:19 -0300

    Simple query using @Query

--- src/main/java/com/luiz/workshop_mongo/resources/exceptions/PostResource.java
+++ src/main/java/com/luiz/workshop_mongo/resources/exceptions/PostResource.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URLDecoder;
 import java.util.List;
 
 @RestController
@@ -17,13 +16,13 @@ public class PostResource {
     @Autowired
     private PostService service;
 
-    @RequestMapping(value = "/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
-    @RequestMapping(value = "/titleserarch", method = RequestMethod.GET)
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
